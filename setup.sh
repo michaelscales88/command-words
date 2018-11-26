3a4d400950176bb6eb419951198dac0514fb05cc
mike <michael.scales88@gmail.com>
2018-11-25 23:52:22 -0600

    working on setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,20 @@
-base="sphinxbase-5prealpha.tar.gz"
-if [ -f "$base" ]
-	wget `https://sourceforge.net/projects/cmusphinx/${base}`
+sphinxbase="sphinxbase-5prealpha"
+sphinxbasetar="$sphinxbase.tar.gz"
+if [ ! -f "$sphinxbasetar" ] && [ ! -d "$sphinxbase" ]; then
+	wget `https://sourceforge.net/projects/cmusphinx/files/sphinxbase/5prealpha/${sphinxbasetar}`
+	xar -xvf "$sphinxbasetar"
 fi
-pocketsphinx="pocketsphinx-5prealpha.tar.gz"
-if [ -f "$pocketsphinx" ]
-	wget `https://sourceforge.net/projects/cmusphinx/files/pocketsphinx/5prealpha/${pocketsphinx}`
+
+pocketsphinx="pocketsphinx-5prealpha"
+pocketsphinxtar="$pocketsphinx.tar.gz"
+if [ ! -f "$pocketsphinxtar" ] && [ ! -d "$pocketsphinx" ]; then
+	wget `https://sourceforge.net/projects/cmusphinx/files/pocketsphinx/5prealpha/${pocketsphinxtar}`
+   xar -xvf "$pocketsphinxtar"
 fi
-sphinxtrain="sphinxtrain-5prealpha.tar.gz"
-if [ -f "$sphinxtrain" ]
-	wget `https://sourceforge.net/projects/cmusphinx/files/sphinxtrain/5prealpha/${sphinxtrain}`
+sphinxtrain="sphinxtrain-5prealpha"
+sphinxtraintar="$sphinxtrain.tar.gz"
+if [ ! -f "$sphinxtraintar" ] && [ ! -d "$sphinxtrain" ]; then
+	wget `https://sourceforge.net/projects/cmusphinx/files/sphinxtrain/5prealpha/${sphinxtraintar}`
+   xar -xvf "$sphinxtraintar"
 fi
 
